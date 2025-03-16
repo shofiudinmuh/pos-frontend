d723e8644c67174a87e38add69e57445b657378a
shofiudinmuh <shofiudinmuh@gmail.com>
2025-03-16 09:04:34 +0700

    update : install ract icon

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,41 +1,57 @@
 import React from 'react';
+import { BiSolidReport, BiLogOut } from 'react-icons/bi';
+import { GrTransaction } from 'react-icons/gr';
+import { FaUsers } from 'react-icons/fa';
+import { MdDashboard, MdInventory, MdOutlineStore, MdPointOfSale } from 'react-icons/md';
 
 const Sidebar = () => {
     return (
-        <div className='bg-gray-800 tex-white w-64 min-h-screen p-4'>
+        <div className='bg-blue-600 text-white w-64 min-h-screen p-4'>
+            <div className='flex items-center justify-center mb-8'>
+                {/* logo */}
+                {/* <img src='https://via.placeholder.com/150x50.png?text=Logo+POS' alt='POS Logo' className='w-32' /> */}
+                <MdPointOfSale size={70} />
+            </div>
             <ul>
                 <li className='mb-2'>
-                    <a href='#' className='hover:text-blue-400'>
+                    <a href='#' className='flex items-center hover:text-blue-400'>
+                        <MdDashboard className='mr-2' />
                         Dashboard
                     </a>
                 </li>
                 <li className='mb-2'>
-                    <a href='#' className='hover:text-blue-400'>
+                    <a href='#' className='flex items-center hover:text-blue-400'>
+                        <GrTransaction className='mr-2' />
+                        Transactions
+                    </a>
+                </li>
+                <li className='mb-2'>
+                    <a href='#' className='flex items-center hover:text-blue-400'>
+                        <MdInventory className='mr-2' />
                         Inventory
                     </a>
                 </li>
                 <li className='mb-2'>
-                    <a href='#' className='hover:text-blue-400'>
-                        Sales
-                    </a>
-                </li>
-                <li className='mb-2'>
-                    <a href='#' className='hover:text-blue-400'>
+                    <a href='#' className='flex items-center hover:text-blue-400'>
+                        <BiSolidReport className='mr-2' />
                         Report
                     </a>
                 </li>
                 <li className='mb-2'>
-                    <a href='#' className='hover:text-blue-400'>
+                    <a href='#' className='flex items-center hover:text-blue-400'>
+                        <MdOutlineStore className='mr-2' />
                         Supplier
                     </a>
                 </li>
                 <li className='mb-2'>
-                    <a href='#' className='hover:text-blue-400'>
-                        User
+                    <a href='#' className='flex items-center hover:text-blue-400'>
+                        <FaUsers className='mr-2' />
+                        User Management
                     </a>
                 </li>
                 <li className='mb-2'>
-                    <a href='#' className='hover:text-blue-400'>
+                    <a href='#' className='flex items-center hover:text-blue-400'>
+                        <BiLogOut className='mr-2' />
                         Logout
                     </a>
                 </li>
